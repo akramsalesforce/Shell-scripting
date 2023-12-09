03e6273b0a9222c018cc5d6b816fd3405754735d
Akram, khan <Akram.khan200@gmail.com>
2023-12-09 16:42:32 +0530

    changes made

--- Roboshop/components/common.sh
+++ Roboshop/components/common.sh
@@ -20,6 +20,53 @@ fi
 LOG_FILE=/tmp/roboshop.log
 rm -f $LOG_FILE
 
+APP_USER=roboshop
+
+APP_SETUP() {
+  id ${APP_USER} &>>${LOG_FILE}
+  if [ $? -ne 0 ]; then
+    Print "Add Application User"
+    useradd ${APP_USER} &>>${LOG_FILE}
+    StatCheck $?
+  fi
+  Print "Download App Component"
+  curl -f -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>${LOG_FILE}
+  StatCheck $?
+
+  Print "CleanUp Old Content"
+  rm -rf /home/${APP_USER}/${COMPONENT} &>>${LOG_FILE}
+  StatCheck $?
+
+  Print "Extract App Content"
+  cd /home/${APP_USER} &>>${LOG_FILE} && unzip -o /tmp/${COMPONENT}.zip &>>${LOG_FILE} && mv ${COMPONENT}-main ${COMPONENT} &>>${LOG_FILE}
+  StatCheck $?
+}
+
+
+SERVICE_SETUP() {
+  Print "Fix App User Permissions"
+  chown -R ${APP_USER}:${APP_USER} /home/${APP_USER}
+  StatCheck $?
+
+  Print "Setup SystemD File"
+  sed -i  -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' \
+          -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' \
+          -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' \
+          -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' \
+          -e 's/CARTENDPOINT/cart.roboshop.internal/' \
+          -e 's/DBHOST/mysql.roboshop.internal/' \
+          -e 's/CARTHOST/cart.roboshop.internal/' \
+          -e 's/USERHOST/user.roboshop.internal/' \
+          -e 's/AMQPHOST/rabbitmq.roboshop.internal/' \
+          /home/roboshop/${COMPONENT}/systemd.service &>>${LOG_FILE} && mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service  &>>${LOG_FILE}
+  StatCheck $?
+
+  Print "Restart ${COMPONENT} Service"
+  systemctl daemon-reload &>>${LOG_FILE} && systemctl restart ${COMPONENT} &>>${LOG_FILE} && systemctl enable ${COMPONENT} &>>${LOG_FILE}
+  StatCheck $?
+}
+
+
 NODEJS() {
 
   Print "Configure Yum repos"
@@ -39,3 +86,4 @@ NODEJS() {
   SERVICE_SETUP
 
 }
+
