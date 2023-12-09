7a1bc8761c76f582181c1ef9a8be0e2768ae26e0
Akram, khan <Akram.khan200@gmail.com>
2023-12-09 22:42:53 +0530

    changes made

--- Roboshop/components/mongodb.sh
+++ Roboshop/components/mongodb.sh
@@ -2,27 +2,27 @@ source components/common.sh
 
 CHECK_ROOT
 
-PRINT "Configure YUM Repos"
+Print "Configure YUM Repos"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>${LOG_FILE}
-CHECK_STAT $?
+StatCheck $?
 
-PRINT "Install MongoDB"
+Print "Install MongoDB"
 yum install -y mongodb-org &>>${LOG_FILE}
-CHECK_STAT $?
+StatCheck $?
 
-PRINT "Configure MongoDB Service"
+Print "Configure MongoDB Service"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
-CHECK_STAT $?
+StatCheck $?
 
-PRINT "Start MonogDB"
+Print "Start MonogDB"
 systemctl enable mongod &>>${LOG_FILE}  && systemctl restart mongod &>>${LOG_FILE}
-CHECK_STAT $?
+StatCheck $?
 
 
-PRINT "Download Schema"
+Print "Download Schema"
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>${LOG_FILE}
-CHECK_STAT $?
+StatCheck $?
 
-PRINT "Load Schema"
+Print "Load Schema"
 cd /tmp && unzip -o mongodb.zip &>>${LOG_FILE}  && cd mongodb-main && mongo < catalogue.js &>>${LOG_FILE}  && mongo < users.js &>>${LOG_FILE}
-CHECK_STAT $?
+StatCheck $?
