38911fb1f8e2c9aeade23eb4a7892a2bacd6b75a
Akram, khan <Akram.khan200@gmail.com>
2023-12-09 14:30:48 +0530

    changes made

--- Roboshop/components/mongodb.sh
+++ Roboshop/components/mongodb.sh
@@ -5,34 +5,34 @@ source components/common.sh
 
 Print "Setup MongoDB Yum repo"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>${LOG_FILE}
-StatusCheck $?
+StatCheck $?
 
 #curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
 
 
 Print "Installing MongoDB"
 yum install -y mongodb-org  &>>${LOG_FILE}
-StatusCheck $?
+StatCheck $?
 
 Print "Configure Listen Address in MonogBD Configuration"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
-StatusCheck $?
+StatCheck $?
 
 Print "Start MongoDB Service"
 systemctl restart mongod &>>${LOG_FILE} && systemctl enable mongod >>${LOG_FILE}
-StatusCheck $?
+StatCheck $?
 
 Print "Download Schema"
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"  >>${LOG_FILE}
-StatusCheck $?
+StatCheck $?
 
 Print "Extract Schema Zip"
 cd /tmp && unzip -o mongodb.zip >>${LOG_FILE}
-StatusCheck $?
+StatCheck $?
 
 cd mongodb-main
 Print "Load Schema example"
 for component in catalogue users ; do
 mongo < ${component}.js >>${LOG_FILE}
 done
-StatusCheck $?
+StatCheck $?
