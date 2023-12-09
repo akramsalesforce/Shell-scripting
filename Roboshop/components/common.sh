4e0d96ff95131205de3645cb137debbcc7c4fbb2
Akram, khan <Akram.khan200@gmail.com>
2023-12-09 18:31:46 +0530

    changes made

--- Roboshop/components/common.sh
+++ Roboshop/components/common.sh
@@ -49,15 +49,15 @@ SERVICE_SETUP() {
   StatCheck $?
 
   Print "Setup SystemD File"
-  sed -i  -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' \
-          -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' \
-          -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' \
-          -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' \
-          -e 's/CARTENDPOINT/cart.roboshop.internal/' \
-          -e 's/DBHOST/mysql.roboshop.internal/' \
-          -e 's/CARTHOST/cart.roboshop.internal/' \
-          -e 's/USERHOST/user.roboshop.internal/' \
-          -e 's/AMQPHOST/rabbitmq.roboshop.internal/' \
+  sed -i  -e 's/MONGO_DNSNAME/mongodb.awsdevops.tech/' \
+          -e 's/REDIS_ENDPOINT/redis.awsdevops.tech/' \
+          -e 's/MONGO_ENDPOINT/mongodb.awsdevops.tech/' \
+          -e 's/CATALOGUE_ENDPOINT/catalogue.awsdevops.tech/' \
+          -e 's/CARTENDPOINT/cart.awsdevops.tech/' \
+          -e 's/DBHOST/mysql.awsdevops.tech/' \
+          -e 's/CARTHOST/cart.awsdevops.tech/' \
+          -e 's/USERHOST/user.awsdevops.tech/' \
+          -e 's/AMQPHOST/rabbitmq.awsdevops.tech/' \
           /home/roboshop/${COMPONENT}/systemd.service &>>${LOG_FILE} && mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service  &>>${LOG_FILE}
   StatCheck $?
 
