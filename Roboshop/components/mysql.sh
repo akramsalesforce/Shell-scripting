235427ffadc1ac0623b85a119b05a40b6b1e614a
Akram, khan <Akram.khan200@gmail.com>
2023-12-10 19:13:52 +0530

    changes made

--- Roboshop/components/mysql.sh
+++ Roboshop/components/mysql.sh
@@ -5,7 +5,7 @@ source components/common.sh
 Print "Configure YUM Repo"
 curl -f -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>${LOG_FILE}
 #curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
-dnf module disable mysql
+dnf module disable mysql -y
 StatCheck $?
 
 
