8904ef2575564e6e6f4cedc2500d0d7b0ad751c6
Akram, khan <Akram.khan200@gmail.com>
2023-12-11 11:54:12 +0530

    changes made

--- Roboshop/components/mysql.sh
+++ Roboshop/components/mysql.sh
@@ -1,47 +1,44 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 source components/common.sh
-
-Print "Configure YUM Repo"
-curl -f -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>${LOG_FILE}
-#curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
-dnf module disable mysql -y
-StatCheck $?
+checkRootUser
 
 
-Print "Install MySQL"
+ECHO "Setup MySQL Yum repo"
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>${LOG_FILE}
+statusCheck $?
+
+ECHO "Install MySQL Server"
 yum install mysql-community-server -y &>>${LOG_FILE}
-StatCheck $?
+statusCheck $?
 
-Print "Start MySQL Service"
-systemctl enable mysqld &>>${LOG_FILE} && systemctl start mysqld &>>${LOG_FILE}
-StatCheck $?
+ECHO "Start MySQL Service"
+systemctl enable mysqld  &>>${LOG_FILE} && systemctl start mysqld  &>>${LOG_FILE}
+statusCheck $?
 
-echo 'show databases' | mysql -uroot -pRoboShop@1 &>>${LOG_FILE}
-if [ 0 -ne $? ]; then
-  Print "Change Default Root Password"
-  echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('RoboShop@1');" >/tmp/rootpass.sql
-  DEFAULT_ROOT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
-  mysql --connect-expired-password -uroot -p"${DEFAULT_ROOT_PASSWORD}" </tmp/rootpass.sql &>>${LOG_FILE}
-  StatCheck $?
+DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" >/tmp/root-pass.sql
+
+echo show databases | mysql -uroot -pRoboShop@1 &>>$LOG_FILE
+if [ $? -ne 0 ]; then
+  ECHO "Reset MySQL Password"
+  mysql --connect-expired-password -u root -p${DEFAULT_PASSWORD} </tmp/root-pass.sql &>>${LOG_FILE}
+  statusCheck $?
 fi
 
-echo show plugins | mysql -uroot -pRoboShop@1 2>>${LOG_FILE} | grep validate_password &>>${LOG_FILE}
+echo 'show plugins;' | mysql -uroot -pRoboShop@1 2>>${LOG_FILE} | grep validate_password &>>${LOG_FILE}
 if [ $? -eq 0 ]; then
-  Print "Uninstall Password Validate Plugin"
-  echo 'uninstall plugin validate_password;' >/tmp/pass-validate.sql
-  mysql --connect-expired-password -uroot -pRoboShop@1 </tmp/pass-validate.sql  &>>${LOG_FILE}
-  StatCheck $?
+  ECHO "Uninstall Password Validation Plugin"
+  echo "uninstall plugin validate_password;" |  mysql -uroot -pRoboShop@1 &>>$LOG_FILE
+  statusCheck $?
 fi
 
-Print "Download Schema"
-curl -f -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip" &>>${LOG_FILE}
-StatCheck $?
+ECHO "Download Schema"
+cd /tmp
+curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip" &>>$LOG_FILE && unzip -o  /tmp/mysql.zip &>>$LOG_FILE
+statusCheck $?
 
-Print "Extract Schema"
-cd /tmp && unzip -o mysql.zip &>>${LOG_FILE}
-StatCheck $?
-
-Print "Load Schema"
-cd mysql-main && mysql -u root -pRoboShop@1 <shipping.sql &>>${LOG_FILE}
-StatCheck $?
+ECHO "Load Schema"
+cd /tmp/mysql-main
+mysql -u root -pRoboShop@1 <shipping.sql &>>${LOG_FILE}
+statusCheck $?
